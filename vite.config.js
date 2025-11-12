e3b0feff856939c858a3501f185f9caad862190a
HossainShahRahat <shah.rahat.hossain.aiub@gmail.com>
2025-11-12 21:54:45 +0600

    backend link change

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://b12-a10-future-box-server-eta.vercel.app",
         changeOrigin: true,
       },
     },
