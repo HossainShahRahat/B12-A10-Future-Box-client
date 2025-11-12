d6a4ef69b9714a4e6b898cb972cc2e92ef1e2ff5
HossainShahRahat <shah.rahat.hossain.aiub@gmail.com>
2025-11-12 22:27:01 +0600

    Bug fix for fetch

--- src/Components/UpcomingEvents/UpcomingEvents.jsx
+++ src/Components/UpcomingEvents/UpcomingEvents.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
-import { motion } from "framer-motion";
 
 const UpcomingEvents = () => {
   const [events, setEvents] = useState([]);
